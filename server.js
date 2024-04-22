69d689e704c708b0ca7b964f43bb86728c59fddd
sachin-pitamaasweb <sachin.pitamaasweb@gmail.com>
2024-04-22 10:15:25 +0530

    new updates

--- server.js
+++ server.js
@@ -1,28 +1,42 @@
 // server.js
 const express = require('express');
+const nodemailer = require('nodemailer');
 const cors = require('cors');
-const bodyParser = require('body-parser');
-
-const formController = require('./src/controllers/formController');
-
-
 const app = express();
+const port = 8000;
 
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-}));
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 app.use(express.json());
 
-// Middleware
-app.use(bodyParser.json());
 
-app.post('/submit-form',  formController.submitForm);
-const PORT = process.env.PORT || 8000;
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const emailTransporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: `work@tixteen.com`,
+    pass: 'iqexkytojdibsrur'
+  },
+});
+
+app.post('/api/send-email', (req, res) => {
+  const { subject, message, to } = req.body;
+
+  const mailOptions = {
+    from: 'work@tixteen.com',
+    to: to,
+    subject: subject,
+    text: message,
+  };
+
+  emailTransporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error sending email:', error);
+      return res.status(500).json({ error: 'An error occurred while sending the email' });
+    }
+
+    res.json({ message: 'Email sent successfully!' });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
